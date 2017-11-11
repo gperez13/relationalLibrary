8e185772625de54b83146d893c7c8ee281b2130f
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-11-11 11:06:57 -0600

    Added component folders to React App

---
+++ reactlibrary/src/Login/Login.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import './Login.css'
+
+
+
+export class Login extends Component {
+
+
+
+
+
+
+
+	render() {
+		return(
+			<div >
+				<form >
+					<input/>
+					<button>Login</button>
+
+				</form>
+			</div>
+			)
+	}
+}
+
+
+
+
+
+
+export default Login;
